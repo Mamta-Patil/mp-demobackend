d11349b8c0b0f0f30d38256f8ed8b1aec3b8d20f
MamtaPatil <mamtapatil5252@gmail.com>
2025-04-21 12:29:31 +0530

    update

--- Server/config/server.js
+++ Server/config/server.js
@@ -1,4 +1,4 @@
 // const mongoose=require("mogoose");
 const mongoose=require("mongoose")
-const connection=mongoose.connect("mongodb://127.0.0.1:27017/STUDENT_INFORMATION")
+const connection=mongoose.connect(process.env.MONGODBURL)
 module.exports=connection
